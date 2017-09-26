09b35dcb437b901a1e406bcecc948222f086d100
shun9 <hanashun9@gmail.com>
2017-09-26 17:16:10 +0900

    Map????? (+1 squashed commits)
    Squashed commits:
    
    [4ed7dec] Map?????

---
+++ RPG_Maker/Classes/Map/Map.cpp
@@ -0,0 +1,64 @@
+//************************************************/
+//* @file  :Map.cpp
+//* @brief :マップ（タイルの集合体）
+//* @date  :2017/09/26
+//* @author:S.Katou
+//************************************************/
+#include "Map.h"
+#include "TileDataHolder.h"
+
+const int Map::WIDTH = 128;
+const int Map::HEIGHT = 128;
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+Map::Map()
+{
+	m_scrollNum = { 0.0f,0.0f };
+
+	m_map.resize(HEIGHT);
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		m_map[i].resize(WIDTH);
+	}
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+Map::~Map()
+{
+
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void Map::Draw()
+{
+	using namespace ShunLib;
+
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			Vec2 pos(i*Tile::SIZE, j*Tile::SIZE);
+			m_map[i][j].Draw(pos - m_scrollNum, Vec2::One);
+		}
+	}
+}
+
+//bool Map::CanMoveSpecifiedDir(ShunLib::ConstantNumber::DIRECTION_2D dir)
+//{
+//	//指定方向のタイル
+//	Tile* tile = m_contactTile[dir];
+//
+//	//タイルが無ければ進めない
+//	if (tile == nullptr)
+//	{
+//		return false;
+//	}
+//
+//	return tile->CanMove();
+//}
--- RPG_Maker/Classes/Map/Tile.h
+++ RPG_Maker/Classes/Map/Tile.h
@@ -1,62 +1,30 @@
 //************************************************/
 //* @file  :Tile.h
 //* @brief :マップタイル
-//* @date  :2017/09/26
+//* @date  :2017/09/27
 //* @author:S.Katou
 //************************************************/
 #pragma once
-
-#include <SL_ConstantNumber.h>
+#include <SL_Vec2.h>
 
 class Enemy;
 
 class Tile
 {
-	//タイルを繋げる
-	//ゲーム開始時に使用
-	static void LinkTile(Tile** map,int row,int column);
+public:
+	//タイルの1辺の長さ
+	static const float SIZE;
 
 private:
-	//敵のエンカウント率
-	int m_encountRate;
-
-	//出現する敵の構成一覧
-	Enemy** m_enemyGroup;
-
-	//接しているタイル　上下左右
-	Tile* m_contactTile[ShunLib::ConstantNumber::DIRECTION_2D::num];
-
-	//プレイヤーが歩くことが出来るかどうか
-	//歩けるなら true
-	bool m_canMove;
+	int m_id;
 
 public:
-	Tile() {}
+	Tile():m_id(-1) {}
 	~Tile() {}
 
-	//エンカウントするかどうか
-	//タイルの上に移動したときに使用
-	bool IsEncount();
+	//描画
+	void Draw(const ShunLib::Vec2& pos, const ShunLib::Vec2& scale);
 
-	//敵グループからランダムに取得
-	Enemy* GetRandamEnemy();
-
-	//指定した方向に移動できるかどうか
-	//タイルの上から移動しようとしたときに使用
-	bool CanMoveSpecifiedDir(ShunLib::ConstantNumber::DIRECTION_2D);
-
-	//指定方向のタイルの情報を設定
-	void SetContactTile(ShunLib::ConstantNumber::DIRECTION_2D,Tile*);
-
-	//Get & Set
-	int EncountRate() { return m_encountRate; }
-	void EncountRate(int rate) {
-		m_encountRate = rate;
-		if (m_encountRate > 100){
-			m_encountRate = 100;
-		}
-	}
-
-	bool CanMove() { return m_canMove; }
-	void CanMove(bool isWalk) { m_canMove = isWalk; }
+	void Id(int id) { m_id = id; }
+	int Id() { return m_id; };
 };
