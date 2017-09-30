bd8fe338a76ca8219739e236a3562812e3bd4c9e
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2017-09-30 14:42:46 +0900

    Create EnemyTable Class

---
+++ RPG_Maker/Classes/UI/Editor/EnemyData/UIEnemyTable.cpp
@@ -0,0 +1,38 @@
+////************************************************/
+////* @file  :UITileProperty.cpp
+////* @brief :UIタイルウインドウの基底クラス
+////* @date  :2017/09/27
+////* @author:K.Yamamoto
+////************************************************/
+//#include <SL_Texture.h>
+//
+//#include "../../../imgui/imgui_impl_dx11.h"
+//#include <vector>
+//#include <SL_Texture.h>
+//#include "UIEnemyTable.h"
+//
+//using namespace ShunLib;
+//using namespace std;
+//
+//UIEnemyTable::UIEnemyTable(const string& name)
+//	:UIBase(name)
+//	,m_isView(true)
+//{
+//}
+//
+//UIEnemyTable::~UIEnemyTable()
+//{
+//}
+//
+//void UIEnemyTable::SetID(int id)
+//{
+//}
+//
+//void UIEnemyTable::UIUpdate()
+//{
+//}
+//
+//void UIEnemyTable::DrawUpdate()
+//{
+//}
+//
