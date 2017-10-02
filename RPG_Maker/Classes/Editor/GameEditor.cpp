355760211a97de052b2b0458d25d3716ee3ccc68
ゆうと <yasasuto0702@gmail.com>
2017-10-02 11:33:36 +0900

    競合時の修正忘れ更新

--- RPG_Maker/Classes/Editor/GameEditor.cpp
+++ RPG_Maker/Classes/Editor/GameEditor.cpp
@@ -135,13 +135,6 @@ void GameEditor::Update()
 	PlayerScroll();
 
 	m_map->SetChoiseId(edi->Id());
-
-	//プレイヤーが先に進めるかどうか
-	if (m_map->CanMoveSpecifiedDir(player->Getpos(), player->Getdirection()) || player->Movestate())
-	{
-		player->Move();
-	}
-	player->Update();
 }
 
 //描画
